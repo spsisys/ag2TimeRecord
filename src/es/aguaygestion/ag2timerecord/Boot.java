262c5a852a43a434b9b29120451a47b01c6c9f4b
spsisys <spsisys@gmail.com>
2013-04-25 17:20:19 +0200

    Main Window display at right place

--- src/es/aguaygestion/ag2timerecord/Boot.java
+++ src/es/aguaygestion/ag2timerecord/Boot.java
@@ -1,6 +1,5 @@
 package es.aguaygestion.ag2timerecord;
 
-import es.aguaygestion.ag2timerecord.MySQLAccess;
 import es.aguaygestion.ag2timerecord.XMLParser;
 
 public class Boot {
@@ -11,29 +10,18 @@ public class Boot {
 	public static void main(String[] args) throws Exception {
 		// Console exit message
 		String exitMessage = null;
-		String hola = null;
-
-		// Display Main Window
-		try {
-			TimeRecordWindow frame = new TimeRecordWindow();
-			frame.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 
 		System.out.println("ag2TimeRecord Client Started.");
-		
+
 		XMLParser xml = new XMLParser();
 		if (xml.readAppXML(Global.appPath, Global.xmlAppFile)) {
 			if (xml.readUsrXML(Global.usrPath, Global.xmlUsrFile)) {
-				MySQLAccess dao = new MySQLAccess();
-				// dao.readWorkers();
-				if (dao.writeTimeRecord()) {
-					exitMessage = "\nTime record saved for worker "
-							+ Global.worker_user + " id " + Global.worker_id
-							+ ". Finished.";
-				} else {
-					exitMessage = "\nError writing Time Record for current user!";
+				// Display Main Window
+				try {
+					TimeRecordWindow frame = new TimeRecordWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			} else {
 				exitMessage = "\nError reading/writing User XML file!";
@@ -41,7 +29,7 @@ public class Boot {
 		} else {
 			exitMessage = "\nError reading Application XML file!";
 		}
-		
+
 		System.out.println(exitMessage);
 	}
 
