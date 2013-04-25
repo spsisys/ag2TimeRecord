33205108c35149cb42c93319488ac0ebcf46290d
spsisys <spsisys@gmail.com>
2013-04-25 18:49:50 +0200

    Button to register time records added

--- src/es/aguaygestion/ag2timerecord/TimeRecordWindow.java
+++ src/es/aguaygestion/ag2timerecord/TimeRecordWindow.java
@@ -3,10 +3,16 @@ package es.aguaygestion.ag2timerecord;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import es.aguaygestion.ag2timerecord.MySQLAccess;
 
 public class TimeRecordWindow {
 
 	public JFrame frmAgtimerecordMarcajes;
+	public MySQLAccess dao = new MySQLAccess();
 
 	/**
 	 * Launch the application.
@@ -35,12 +41,27 @@ public class TimeRecordWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
 		frmAgtimerecordMarcajes = new JFrame();
 		frmAgtimerecordMarcajes.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmAgtimerecordMarcajes.setTitle("ag2TimeRecord | Marcajes");
 		frmAgtimerecordMarcajes.setResizable(false);
 		frmAgtimerecordMarcajes.setBounds(100, 100, 450, 300);
 		frmAgtimerecordMarcajes.getContentPane().setLayout(null);
+		
+		JButton btnRegistrarMarcaje = new JButton("Registrar Marcaje");
+		btnRegistrarMarcaje.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					dao.writeTimeRecord();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		btnRegistrarMarcaje.setBounds(6, 6, 152, 29);
+		frmAgtimerecordMarcajes.getContentPane().add(btnRegistrarMarcaje);
 	}
 
 }
